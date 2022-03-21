8b74c45369959a20d731fd85e69af8f9d7a77a8f
Sadeep Weerasinghe <darshanasadeep@gmail.com>
2022-03-21 05:41:23 +0530

    Dash instead of zero for minted count when not connected

--- src/App.js
+++ src/App.js
@@ -154,7 +154,7 @@ function App() {
                 color: "var(--accent-text)",
               }}
             >
-              {data.totalSupply} / {CONFIG.MAX_SUPPLY}
+              {data.totalSupply || '-'} / {CONFIG.MAX_SUPPLY}
             </s.TextTitle>
             <s.TextDescription
               style={{
